94fe2005272a26e3b9bed9028a730cc316a71b75
Mehmet ORTABURUN <mehmet.ortaburun@gmail.com>
2022-03-07 01:51:36 +0300

    Swagger eklendi.

--- northwind/src/main/java/kioProject/northwind/NorthwindApplication.java
+++ northwind/src/main/java/kioProject/northwind/NorthwindApplication.java
@@ -3,11 +3,16 @@ package kioProject.northwind;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 @SpringBootApplication
+@EnableSwagger2
 public class NorthwindApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NorthwindApplication.class, args);
 	}
+
 
 }
